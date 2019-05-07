a4547987a04cf80728d7b8e759b173d792a45315
WHYSOEASY <48154623+WHYSOEASY@users.noreply.github.com>
2019-05-07 20:57:09 +0100

    Update update_linux.sh

--- update_linux.sh
+++ update_linux.sh
@@ -7,4 +7,5 @@ echo "Moving to directory which was created"
 git clone https://github.com/WHYSOEASY/instaInstaller.git
 cd instaInstaller
 echo "Update complete"
+echo "Make sure to do cd .. then go back on it and updated files will be there"
 
