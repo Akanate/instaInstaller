8eb5460ce7d3a43a796ad74d347b6e342c78cf5d
WHYSOEASY <48154623+WHYSOEASY@users.noreply.github.com>
2019-05-07 20:52:41 +0100

    Update update_linux.sh

--- update_linux.sh
+++ update_linux.sh
@@ -3,16 +3,14 @@ echo "Removing old files"
 cd "$(dirname "$0")"
 rm -r ak_installer.py
 rm -r list.txt
-mkdir update_files
-echo "Making directory for store"
-cd update_files
+rm -r README.md
+rm -r update.py
+rm -r update_linux.sh
 echo "Moving to directory which was created"
 git clone https://github.com/WHYSOEASY/instaInstaller.git
 cd instaInstaller
 echo "Moving to repository directory"   
 echo "Updated"
-mv ak_installer.py "$(dirname "$0")"
-mv list.txt "$(dirname "$0")"
 rm -r update_files
 echo "Put new files in"
 echo "Cleaning up"
