9073fe960e877c5eb148aff0df0197dd637d74f3
WHYSOEASY <48154623+WHYSOEASY@users.noreply.github.com>
2019-05-07 17:55:41 +0100

    Update update_linux.sh

--- update_linux.sh
+++ update_linux.sh
@@ -9,7 +9,8 @@ echo "Moving to repository directory"
 echo "Updated"
 cd ..
 cd ..
-mv /root/update_files/instaInstaller/ak_installer.py "$(dirname "$0")" 
+mv /root/update_files/instaInstaller/ak_installer.py "$(dirname "$0")"
+mv /root/update_files/instaInstaller/list.txt
 rm -r update_files
 echo "Cleaning up"
 echo "Update complete"
